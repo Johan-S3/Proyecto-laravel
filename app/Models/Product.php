306c39c3781691c1bbcce96802d06064068ccc91
Johan Polo <johan.s310806@gmail.com>
2025-06-11 18:16:54 -0500

    Relaciones

---
+++ app/Models/Product.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasOne;
+
+// use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Product extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'price',
+        'stock',
+        'category_id',
+        'image_id'
+    ];
+
+    public function category()
+    {
+        return $this->belongsTo("Category");
+    }
+
+    public function image(): HasOne
+    {
+        return $this->hasOne("Image","image_id");
+    }
+}
