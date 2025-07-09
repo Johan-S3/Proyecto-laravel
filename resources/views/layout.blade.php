9a5b258045cf450e74c55ab4d705f48613dcd61c
Johan Polo <johan.s310806@gmail.com>
2025-07-08 21:35:26 -0500

    Diseño de layout de posts

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -1,12 +1,19 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es">
 <head>
+  @vite('resources/css/app.css')  
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <title>Document</title>
 </head>
 <body>
-  
+  <header>@yield('header')</header>
+  <main class="grid grid-cols-4 gap-4 justify-center w-screen">
+    @yield('main')
+  </main>
+  <footer>
+    @yield('footer')
+  </footer>
 </body>
 </html>
--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,14 +1,14 @@
-@extends('layout');
+@extends('layout')
 
 @section('header')
-    <h1>Posts</h1>
+    <h1 class="font-mono text-5xl font-bold text-center">Posts</h1>
 @endsection
 
 @section('main')
     @foreach ($posts as $item)
-      <div>
+      <div class="w-80 p-10 min-w-64 rounded-3xl border border-black shadow-xl">
         <img src="{{$item->image->path}}" alt="imagen">
-        <h1>{{$item->title}}</h1>
+        <h1 class="text-5xl text-red-500 font-bold">{{$item->title}}</h1>
         <p>{{$item->body}}</p>
       </div>
     @endforeach
